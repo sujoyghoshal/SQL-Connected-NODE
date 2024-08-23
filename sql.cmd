4e42396c3112fb4ae5fbea4270399f95862db50c
sujoyghoshal <sujoyghoshal.s@gmail.com>
2024-08-23 22:26:56 +0530

    adding sql connected whith Node

---
+++ sql.cmd
@@ -0,0 +1,13 @@
+--create database coding;
+
+-- use coding;
+
+-- CREATE TABLE collage (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255),
+--     email VARCHAR(255),
+--     age INT NOT NULL,
+--     dept VARCHAR(50)
+-- );
+
+SELECT * FROM collage;
